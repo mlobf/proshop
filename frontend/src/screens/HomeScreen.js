e38365b605524bbaa1fb6235083cd13e845420c7
mlobf <marcoslemeborbafilho@gmail.com>
2021-10-17 13:55:29 -0300

    Add axios async await

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from "../products";
+//import products from "../products";
+import axios from 'axios';
 import Product from "../components/Product";
 
-function HomeScreen ()
-{
+function HomeScreen() {
+    const [products, setProducts] = useState([]);
+
+    // Using axios and async. To update the state.
+    useEffect(() => {
+        async function fetchProducts() {
+            const { data } = await axios.get('http://127.0.0.1:8000/api/products/');
+            setProducts(data);
+        }
+        fetchProducts();
+    }, []);
+
+
     return (
         <div>
             <h1>Latest Products</h1>
             <Row>
-                { products.map( product => (
-                    <Col key={ product._id } sm={ 6 } md={ 6 } Lg={ 6 } xL={ 6 }>
-                        <Product product={ product } />
+                {products.map(product => (
+                    <Col key={product._id} sm={6} md={5} lg={4} xl={3}>
+                        <Product product={product} />
                     </Col>
-                ) )
+                ))
 
                 }
             </Row>
